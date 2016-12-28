3b5285e06592787ecdbe82a727cb73914996961e
ice <smalltides@gmail.com>
2016-12-28 23:20:10 +0800

    add User Model

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -4,5 +4,5 @@ mongoose.connect('mongodb://127.0.0.1/user_test');
 mongoose.connection
   .once('open', () => console.log('mongo connected!'))
   .on('error', (error) => {
-    console.warn('Error', error);
+    console.warn('Warning', error);
   });
