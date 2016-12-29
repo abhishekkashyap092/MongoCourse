2ff216eaf18610963fd3825f9d941e33e5144826
ice <smalltides@gmail.com>
2016-12-30 02:03:06 +0800

    use nodemon to monitor test change

--- users/test/create_test.js
+++ users/test/create_test.js
@@ -1,8 +1,8 @@
 const assert = require('assert');
 const User = require('../src/user');
 
-describe('Creating records', (done) => {
-  it('save a user', () => {
+describe('Creating records', () => {
+  it('save a user', (done) => {
     const joe = new User({ name: 'Joe' });
 
     joe.save()
--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1/user_test');
-mongoose.connection
-  .once('open', () => console.log('mongo connected!'))
-  .on('error', (error) => {
-    console.warn('Warning', error);
-  });
+mongoose.Promise = global.Promise;
+
+before((done) => {
+  mongoose.connect('mongodb://127.0.0.1/user_test');
+  mongoose.connection
+    .once('open', () => { done(); })
+    .on('error', (error) => {
+      console.warn('Warning', error);
+    });
+});
 
 beforeEach((done) => {
   mongoose.connection.collections.users.drop(() => {
