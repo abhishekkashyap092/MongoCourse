e193bea62ff56e4f82c470787b6d6a01f79cf3d1
ice <smalltides@gmail.com>
2017-01-05 15:39:41 +0800

    add model route and controller

--- muber/app.js
+++ muber/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const routes = require('./routes/routes');
+
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/api', (req, res) => {
-  res.send({ hi: 'there' });
-});
+routes(app);
 
 module.exports = app;
