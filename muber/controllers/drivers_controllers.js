d065614526ea1408d5108caaedd8870594e5dd88
ice <smalltides@gmail.com>
2017-01-05 18:16:38 +0800

    test post to /api/drivers

--- muber/controllers/drivers_controllers.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  greeting(req, res) {
-    res.send({ hi: 'there' });
-  },
-  create(req, res) {
-
-  }
-};
--- muber/routes/routes.js
+++ muber/routes/routes.js
@@ -1,4 +1,4 @@
-const DriversController = require('../controllers/drivers_controllers');
+const DriversController = require('../controllers/drivers_controller');
 
 module.exports = (app) => {
   app.get('/api', DriversController.greeting);
